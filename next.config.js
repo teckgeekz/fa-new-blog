4d8712a16c967e58d7a5859ce23d795b36e8a3d1
teckgeekz <teckgeekz@users.noreply.github.com>
2023-11-11 08:55:28 +0000

    Initialize Edgio

--- next.config.js
+++ next.config.js
@@ -1,3 +1,7 @@
+// This file was automatically added by edgio init.
+// You should commit this file to source control.
+const { withEdgio } = require('@edgio/next/config')
+
 /**
  * @type {import('next').NextConfig}
  */
@@ -13,5 +17,10 @@ const nextConfig = {
   // Optional: Change the output directory `out` -> `dist`
   // distDir: 'dist',
 }
- 
-module.exports = nextConfig
+
+const _preEdgioExport = nextConfig;;
+
+module.exports = (phase, config) =>
+  withEdgio({
+    ..._preEdgioExport
+  })
