e9b223ad3efc8bd5db61781e0cc521598b16cf9f
teckgeekz <jeoffrey.mathews@jmgcapital.net>
2023-12-13 18:57:38 +0530

    Update SEO.js

--- components/SEO.js
+++ components/SEO.js
@@ -5,6 +5,7 @@ export default function SEO({ title, description }) {
     <Head>
       <title>{title}</title>
     <meta name="google-site-verification" content="Svqj5pHGiV9S8pYpkkDmQldmX2Jqaavu18oMvlNx0Ik" />
+    <meta name="google-site-verification" content="taxkt9dmuH03MIA0BDi6wkTiYwZLQR20CWP76SRlgJc" />
       <meta name="description" content={description} />
       <meta property="og:title" content={title} />
     </Head>
